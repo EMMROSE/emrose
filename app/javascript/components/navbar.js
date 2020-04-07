66d96a5eb0afe34d7770e5c5172495bb007c9b23
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-04-07 19:10:34 +0200

    parralax

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,44 +1,14 @@
 const scrolledNav = () => {
   const navbar = document.querySelector('#js-scroll-navbar');
-  const buttons = document.querySelectorAll('#js-btn-white');
-  const navbarBrand = document.querySelector('#js-navbar-brand');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 380) {
-        navbarSearch.classList.remove('d-none');
+      if (window.scrollY >= 540) {
         navbar.classList.remove('bg-transparent');
-        buttons.forEach(button => {
-          button.classList.remove('white-btn')
-        })
-        if (balance) {
-          balance.classList.remove('text-white');
-          balance.classList.remove('border-white');
-        }
       } else {
-        navbarSearch.classList.add('d-none');
         navbar.classList.add('bg-transparent');
-        buttons.forEach(button => {
-            button.classList.add('white-btn')
-        })
-        if (balance) {
-          balance.classList.add('text-white');
-          balance.classList.add('border-white');
-        }
-      }
-
-      if (window.scrollY >= 220) {
-        navbarSearch.classList.remove('d-none');
-        bannerSearch.classList.add('d-none');
-        navbarBrand.classList.remove('flex-grow-1');
-      } else {
-        navbarSearch.classList.add('d-none');
-        bannerSearch.classList.remove('d-none');
-        navbarBrand.classList.add('flex-grow-1');
       }
     });
   }
 }
-
-
 
 export { scrolledNav }
