aa323833f73a93dbb8ac4b1bb54820a6b7419a87
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-06-04 16:56:49 +0200

    home

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < ApplicationRecord
+  validates :message, presence: true
+  validates :name, presence: true
+  validates :email,
+    :presence => :true,
+    :format => {
+      :with => /\S+@\S+\.\S+/i,
+      :message => "le format de l'adresse e-mail doit être correct."
+    }
+  validates :phone,
+    :presence => :true,
+    :format => {
+      :with => /(0|\+33[\s-]?)[1-9][\s-]?(\d{2}[\s-]?){4}^/,
+      :message => "votre n° de téléphone est mal renseigné."
+    }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_175204) do
+ActiveRecord::Schema.define(version: 2020_06_04_132706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "message"
+    t.string "email"
+    t.string "phone"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
