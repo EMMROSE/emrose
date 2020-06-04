af4518fb67a9392d7f1c0cd5d6912b493f4f05dc
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-06-04 12:02:36 +0200

    home

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const scrolledNav = () => {
   const navbar = document.querySelector('#js-scroll-navbar');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 540) {
+      if (window.scrollY >= 450) {
         navbar.classList.remove('bg-transparent');
       } else {
         navbar.classList.add('bg-transparent');
