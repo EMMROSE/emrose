5e91f86b6d896a1dd760df413c1c56b62d18b373
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-09-28 21:14:41 +0200

    new JS scroll function

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -24,6 +24,31 @@ const scrolledFx = () => {
         }
       });
     }
+} else if (window.matchMedia("(max-width: 900px)").matches) {
+  /* the view port is less than 900 pixels wide */
+  const form = document.querySelector('#js-scroll');
+    if (form) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= 450) {
+          // const x = scrollY - 330
+          form.classList.add('margin-form1');
+          // form.style.setProperty('top', `${x*0.5}px`)
+          // form.style.setProperty('left', `-${x}px`)
+       } else {
+          form.classList.remove('margin-form1');
+        }
+      });
+    }
+    const mockup = document.querySelector('#js-scroll-mockup');
+    if (mockup) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= 450) {
+          mockup.classList.add('mockupmove1');
+        } else {
+          mockup.classList.remove('mockupmove1');
+        }
+      });
+    }
 } else if (window.matchMedia("(min-width: 900px)").matches) {
   /* the view port is less than 900 pixels wide */
   const form = document.querySelector('#js-scroll');
@@ -71,6 +96,27 @@ const scrolledFx = () => {
         }
       });
     }
+  } else if (window.matchMedia("(max-width: 900px)").matches) {
+      const formbis = document.querySelector('#js-scroll-bis');
+      if (formbis) {
+        window.addEventListener('scroll', () => {
+          if (window.scrollY >= 800) {
+            formbis.classList.add('margin-form2');
+          } else {
+            formbis.classList.remove('margin-form2');
+          }
+        });
+      }
+      const mockupbis = document.querySelector('#js-scroll-mockupbis');
+      if (mockupbis) {
+        window.addEventListener('scroll', () => {
+          if (window.scrollY >= 800) {
+            mockupbis.classList.add('mockupmove2');
+          } else {
+            mockupbis.classList.remove('mockupmove2');
+          }
+        });
+      }
   } else if (window.matchMedia("(min-width: 900px)").matches) {
       const formbis = document.querySelector('#js-scroll-bis');
       if (formbis) {
@@ -108,6 +154,27 @@ const scrolledFx = () => {
     if (mockupter) {
       window.addEventListener('scroll', () => {
         if (window.scrollY >= 800) {
+          mockupter.classList.add('mockupmove1');
+        } else {
+          mockupter.classList.remove('mockupmove1');
+        }
+      });
+    }
+  } else if (window.matchMedia("(max-width: 900px)").matches) {
+    const formter = document.querySelector('#js-scroll-ter');
+    if (formter) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= 1250) {
+          formter.classList.add('margin-form1');
+        } else {
+          formter.classList.remove('margin-form1');
+        }
+      });
+    }
+    const mockupter = document.querySelector('#js-scroll-mockupter');
+    if (mockupter) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= 1250) {
           mockupter.classList.add('mockupmove1');
         } else {
           mockupter.classList.remove('mockupmove1');
@@ -157,6 +224,27 @@ const scrolledFx = () => {
         }
       });
     }
+  } else if (window.matchMedia("(max-width: 900px)").matches) {
+      const formfour = document.querySelector('#js-scroll-four');
+      if (formfour) {
+        window.addEventListener('scroll', () => {
+          if (window.scrollY >= 1700) {
+            formfour.classList.add('margin-form2');
+          } else {
+            formfour.classList.remove('margin-form2');
+          }
+        });
+      }
+      const mockupbis = document.querySelector('#js-scroll-mockupfour');
+      if (mockupbis) {
+        window.addEventListener('scroll', () => {
+          if (window.scrollY >= 1700) {
+            mockupbis.classList.add('mockupmove2');
+          } else {
+            mockupbis.classList.remove('mockupmove2');
+          }
+        });
+      }
   } else if (window.matchMedia("(min-width: 900px)").matches) {
       const formfour = document.querySelector('#js-scroll-four');
       if (formfour) {
